d13588d9887a1be6c468ca0fd54cbf2cb6dced8b
muntasir17 <muntasirshuvo791@gmail.com>
2026-08-24 14:03:19 +0600

    adding

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const windowHeight = window.innerHeight;
             reveals.forEach((el) => {
                 const elementTop = el.getBoundingClientRect().top;
-                const revealPoint = 80; // স্ক্রিনের ৮০ পিক্সেল কাছে আসলে অ্যানিমেশন স্টার্ট হবে
+                const revealPoint = 80;
 
                 if (elementTop < windowHeight - revealPoint) {
                     el.classList.add("active");
@@ -19,7 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         };
 
-        // স্ক্রোল করলে এবং পেজ প্রথমবার লোড হলেই চেক করবে
         window.addEventListener("scroll", revealOnScroll);
         revealOnScroll();
     });
@@ -27,7 +26,7 @@ const sr = ScrollReveal({
         distance: '60px',
         duration: 1000,
         easing: 'cubic-bezier(0.5, 0, 0, 1)',
-        reset: false // একবার স্ক্রোল করলে অ্যানিমেশন ফিক্সড থাকবে
+        reset: false 
     });
 
     sr.reveal('.sr-title', {
@@ -35,21 +34,31 @@ const sr = ScrollReveal({
         delay: 200
     });
 
-    // প্যারাগ্রাফ আরও একটু পরে নিচ থেকে আসবে
     sr.reveal('.sr-desc', {
         origin: 'bottom',
         delay: 400
     });
 
-    // বাটনগুলো ৪ নম্বর ধাপে আসবে
     sr.reveal('.sr-btn', {
         origin: 'bottom',
         delay: 600
     });
 
-    // ডানপাশের কার্ডটি ডান দিক থেকে স্মুথভাবে স্লাইড হয়ে আসবে
     sr.reveal('.sr-card', {
         origin: 'right',
         distance: '80px',
         delay: 700
     });
+
+
+
+
+
+
+
+
+
+
+
+
+    
